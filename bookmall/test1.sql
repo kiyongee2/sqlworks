407be918a2cbadba0b34a06664f3b3da9ec0d356
kiyongee2 <kiyongee2@gmail.com>
2023-06-12 11:45:04 +0900

    web db

---
+++ bookmall/test1.sql
@@ -0,0 +1,34 @@
+SELECT * FROM book;
+
+-- bookid가 11인 도서 삭제
+DELETE FROM book WHERE bookid = 11;
+
+-- 서브 쿼리(쿼리 중첩)
+-- 책의 총수량을 구하시오
+SELECT COUNT(*) 총수량 FROM book;
+
+-- 도서중 가장 높은 가격을 찾으시오
+SELECT MAX(price) 최고가격 FROM book;
+
+-- 가장 비싼 도서의 이름을 검색하시오
+SELECT bookname, price 
+FROM book
+WHERE price = (SELECT MAX(price) 최고가격 FROM book);
+
+-- 고객과 고객의 주문에 관한 데이터를 모두 검색하시오
+SELECT * FROM customer;
+SELECT * FROM orders;
+
+-- 김연아 고객의 총 주문 가격을 검색하시오
+SELECT cus.name, SUM(saleprice)
+FROM customer cus, orders ord
+WHERE cus.custid = ord.custid
+GROUP BY cus.name
+HAVING cus.name='김연아';
+
+
+
+
+
+
+
