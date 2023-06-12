5714485e3471a42fe7e2b6817f33fb83861d18df
kiyongee2 <kiyongee2@gmail.com>
2023-06-12 18:20:06 +0900

    t_board

--- mydb/t_member.sql
+++ mydb/t_member.sql
@@ -6,8 +6,27 @@ CREATE TABLE t_member(
     gender      VARCHAR2(10),
     joindate    DATE DEFAULT SYSDATE
 );
+-- 게시판 테이블 생성
+CREATE TABLE t_board(
+    bnum    NUMBER(4) PRIMARY KEY,
+    title   VARCHAR2(100) NOT NULL,
+    content VARCHAR2(2000) NOT NULL,
+    regdate TIMESTAMP DEFAULT SYSTIMESTAMP,
+    modifydate TIMESTAMP,
+    hit NUMBER DEFAULT 0,
+    memberid VARCHAR2(20) NOT NULL,
+    CONSTRAINT FK_MemberBoard FOREIGN KEY(memberid)
+    REFERENCES t_member(memberid)
+);
+-- 글번호, 글제목, 글내용, 작성일, 수정일, 조회수, 아이디(외래키)
+-- 자동 순번(SEQUENCE)
+CREATE SEQUENCE b_seq;
 
 INSERT INTO t_member(memberid, passwd, name, gender)
 VALUES ('cloud', 'm12345', '김기용', '남');
+
+--INSERT INTO t_board()
+
+
 
 SELECT * FROM t_member;
