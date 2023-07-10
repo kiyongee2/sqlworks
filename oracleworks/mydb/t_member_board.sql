fe348d22c58a5b53af6bec5ab50df09fbabc9e1b
kiyongee2 <kiyongee2@gmail.com>
2023-07-10 18:20:41 +0900

    mysql works

--- oracleworks/mydb/t_member_board.sql
+++ oracleworks/mydb/t_member_board.sql
@@ -19,8 +19,17 @@ CREATE TABLE t_board(
     REFERENCES t_member(memberid) ON DELETE CASCADE
 );
 -- 글번호, 글제목, 글내용, 작성일, 수정일, 조회수, 아이디(외래키)
--- 자동 순번(SEQUENCE)
-CREATE SEQUENCE b_seq;
+-- 자동 순번(SEQUENCE) 전체 코드(CACHE 기본값 20 - 캐시 사용안함)
+CREATE SEQUENCE b_seq
+INCREMENT BY 1
+START WITH 1
+MINVALUE 1
+MAXVALUE 9999
+NOCYCLE
+NOCACHE;
+
+-- 파일 업로드 칼럼 추가
+ALTER TABLE t_board ADD fileupload VARCHAR2(100);
 
 INSERT INTO t_member(memberid, passwd, name, gender)
 VALUES ('cloud', 'm12345', '김기용', '남');
@@ -31,6 +40,30 @@ VALUES (b_seq.NEXTVAL, '가입인사', '안녕하세요. 반갑습니다.', 'cloud');
 COMMIT;
 SELECT * FROM t_member;
 SELECT * FROM t_board;
+
+-- id 중복 체크
+SELECT COUNT(*) AS result 
+FROM t_member WHERE memberid = 'cloud5';
+
+-- id 중복 체크
+SELECT DECODE(COUNT(*), 1, 'true', 'false') AS result 
+FROM t_member WHERE memberid = 'cloud';
+
+-- 페이지 처리
+SELECT ROWNUM, t_board.*  
+FROM t_board
+WHERE ROWNUM >= 1 AND ROWNUM <= 10
+ORDER BY bnum; 
+
+-- 페이지 처리(인라인뷰 - 중첩쿼리(서브쿼리))
+SELECT *
+FROM (SELECT ROWNUM rn, t_board.*  FROM t_board ORDER BY bnum DESC)
+WHERE rn > 0 AND RN <= 10;  --별칭(RN)을 사용해야 가능
+
+-- 총 행의 수
+SELECT COUNT(*) FROM t_board;
+
+
 
 DROP TABLE t_board;  -- board 테이블 삭제
 DROP SEQUENCE b_seq; -- 시퀀스 삭제
